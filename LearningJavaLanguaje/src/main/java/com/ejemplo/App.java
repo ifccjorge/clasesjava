66b82a9cd3487c7d1f7ac04af82d5c3178d399de
jorge <jorge@dominio.com>
2026-04-08 14:58:44 +0200

    Modificadores de acceso

--- LearningJavaLanguaje/src/main/java/com/ejemplo/App.java
+++ LearningJavaLanguaje/src/main/java/com/ejemplo/App.java
@@ -1,17 +1,9 @@
 package com.ejemplo;
 
-/**
- * Hello world!
- */
 public class App {
     public static void main(String[] args) {
     	Persona persona1 = new Persona();
-    	persona1.nombre = "Dani";
-        System.out.println("Persona: " + persona1.nombre + " - " + persona1);
-    	Persona persona2 = new Persona();
-    	persona2.nombre = "Dani";
-    	Persona persona3 = persona1;
-    	System.out.println("Iguales: " + (persona1 == persona2));
-    	System.out.println("Iguales: " + (persona1 == persona3));
+    	persona1.setNombre("Dani");
+        System.out.println("Persona: " + persona1.getNombre());
     }
 }
--- LearningJavaLanguaje/src/main/java/com/ejemplo/Persona.java
+++ LearningJavaLanguaje/src/main/java/com/ejemplo/Persona.java
@@ -1,8 +1,25 @@
 package com.ejemplo;
 
 public class Persona {
-	String nombre;
-	String primerApellido;
-	String segundoApellido;
-
+	private String nombre;
+	private String primerApellido;
+	private String segundoApellido;
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getPrimerApellido() {
+		return primerApellido;
+	}
+	public void setPrimerApellido(String primerApellido) {
+		this.primerApellido = primerApellido;
+	}
+	public String getSegundoApellido() {
+		return segundoApellido;
+	}
+	public void setSegundoApellido(String segundoApellido) {
+		this.segundoApellido = segundoApellido;
+	}
 }
