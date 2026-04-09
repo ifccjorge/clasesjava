11c06a3b02073cc93dea6aeaeb7cddc2cb4ae55f
jorge <jorge@dominio.com>
2026-04-09 13:46:20 +0200

    Notación Lombok en la clase Persona

--- LearningJavaLanguaje/src/main/java/com/ejemplo/Persona.java
+++ LearningJavaLanguaje/src/main/java/com/ejemplo/Persona.java
@@ -1,8 +1,21 @@
 package com.ejemplo;
 
-public class Persona {
+import java.time.LocalDate;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class Persona extends Object{
 	private String nombre;
 	private String primerApellido;
 	private String segundoApellido;
+	private Genero genero;
+	private LocalDate fechaNacimiento;
 	private double salario;
 }
