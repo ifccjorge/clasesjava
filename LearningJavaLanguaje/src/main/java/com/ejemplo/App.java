dcc979a4640823138bdfdc28906589cf7e232be0
jorge <jorge@dominio.com>
2026-04-10 10:30:58 +0200

    Última clase sobre modificadores estáticos

--- LearningJavaLanguaje/src/main/java/com/ejemplo/App.java
+++ LearningJavaLanguaje/src/main/java/com/ejemplo/App.java
@@ -4,20 +4,47 @@ import java.time.LocalDate;
 import java.time.Month;
 
 public class App {
-	public String texto = "Hola";
+	
+	public String texto = "Vivo en el plano de las instancias";
+	
 	public static void main(String[] args) {
-		Persona persona1 = Persona.builder()
-				.nombre("Juana")
-				.salario(30000)
-				.genero(Genero.MUJER)
+		
+		/* Creacion o instanciacion de objetos utilizando el patron Builder de Lombok
+		 * , que no fue creado por Lombok pero si lo han mejorado para su utilizacion.
+		 * 
+		 * El patron Builder es llamado un patron de diseño Factory, es decir, es un patron
+		 * de diseño creacional (para la creacion de objetos). 
+		 * 
+		 * ¿Que ventajas nos aporta?
+		 * 
+		 * Nos permite crear objetos aunque no tengamos el constructor explicitamente 
+		 * implementado, y ademas cuando vamos a crear un objeto el orden en que utilicemos
+		 * las propiedades o campos de la clase es indiferente */
+		
+		Persona p1 = Persona.builder()
+				.salario(3500.25)
+				.nombre("Carolina")
 				.build();
+		
 		Persona persona2 = Persona.builder()
-				.nombre("Luis")
+				.nombre("Sebastian")
+				.primerApellido("Sanjuanelo")
+				.segundoApellido("Arrieta")
+				.genero(Genero.HOMBRE)
 				.fechaNacimiento(LocalDate.of(2000, Month.JANUARY, 20))
+				.salario(4500.25)
 				.build();
-		System.out.println(persona1);
+		
 		System.out.println(persona2);
-		System.out.println(Persona.PAIS);
-		System.out.println(new App().texto);
+		
+		System.out.println("El pais de todas las personas es: " + Persona.PAIS);
+
+		// Mostrar el valor de la variable texto
+		
+	   // App app = new App();
+	   
+	   System.out.println(new App().texto);
+		
+		
 	}
 }
--- LearningJavaLanguaje/src/main/java/com/ejemplo/Persona.java
+++ LearningJavaLanguaje/src/main/java/com/ejemplo/Persona.java
@@ -11,12 +11,25 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Data
 @Builder
-public class Persona {
+public class Persona  {
+
+	/*
+	 * Los elementos miembros de una clase (propiedades, metodos, los constructores)
+	 * solamente pueden existir (vivir) en el plano de las clases o en el plano de las
+	 * instancias
+	 * 
+	 * El elemento que vive, que existe, en el plano de las clases tiene que tener el 
+	 * modificador static, y no necesita de una instancia de la clase para poder existir,
+	 * y se accede al mismo prefejijandolo por el nombre de la clase */
+	
 	public static final String PAIS = "España";
+	
 	private String nombre;
 	private String primerApellido;
 	private String segundoApellido;
 	private Genero genero;
 	private LocalDate fechaNacimiento;
 	private double salario;
+	
+	
 }
